cc6e8594b82e5bee3fa45a91d9e73716b6665d08
mahendraravichandran <180241342+mahendraravichandran@users.noreply.github.com>
2025-11-03 16:39:08 -0500

    Time: 4 ms (4.49%) | Memory: 18 MB (14.52%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,26 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        # what I think is first we can create a list and add each element to a list and when the hashtag is present we can just pop the last element 
+        stackA = []
+        stackB = []
+        testA=""
+        testB=""
+        for i in range(len(s)):
+            if s[i]=="#":
+                if stackA:
+                    stackA.pop()
+            else:
+                stackA.append(s[i]) 
+        for i in t:
+            if i == "#":
+                if stackB:
+                    stackB.pop()
+            else:
+                stackB.append(i)
+        for i in stackA:
+            testA += i
+        for i in stackB:
+            testB += i
+        return testA == testB
+            
+
